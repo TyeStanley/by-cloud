3998b777e78e59afb8dddb870ac521b3ac654769
Tye Stanley <tyestanley090@gmail.com>
2022-10-30 18:57:03 -0700

    file setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config()
+
+
+const app = express()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+const port = process.env.PORT || 3001;
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`app is live on ${port}`)
+})
