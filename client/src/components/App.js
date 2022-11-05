84a94f45cd8f43286fa3a2e7f52739f778ce0ac3
Tye Stanley <tyestanley090@gmail.com>
2022-11-04 19:45:12 -0700

    added react links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/homepage';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
